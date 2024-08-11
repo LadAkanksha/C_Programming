9af8e38e1604fc179aa61715ff41c6000696855a
LadAkanksha <akankshalad2003@gmail.com>
2024-08-11 17:57:31 +0530

    Basic Program

---
+++ Basic/ASCII Table.c
@@ -0,0 +1,16 @@
+///Program to Print ASCII Table
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+
+    for(i=0 ; i<=127 ; i++)
+    {
+        printf("\n%d = %c",i,i);
+    }
+    getch();
+    return 0;
+}
