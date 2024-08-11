491288dc3139fb150f60621875927f89d5cf90f6
LadAkanksha <akankshalad2003@gmail.com>
2024-08-11 18:20:18 +0530

    Basic Program

---
+++ Basic/ASCII Character.c
@@ -0,0 +1,18 @@
+///Program to Find Out ASCII value of Given Character
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int i=0;
+
+    printf("\nEnter Any Number : ");
+    scanf("%d",&i);
+
+    printf("\nASCII of %d = %c",i,i);
+
+    getch();
+    return 0;
+}
