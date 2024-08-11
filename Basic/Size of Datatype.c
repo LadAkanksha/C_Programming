0ad02a29a73c31b3eb638d60a49c651407b2b4d3
LadAkanksha <akankshalad2003@gmail.com>
2024-08-11 17:43:57 +0530

    Basic Program

---
+++ Basic/Size of Datatype.c
@@ -0,0 +1,15 @@
+///program to print size of int,char,float,double
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\nSize of Int = %d",sizeof(int));
+    printf("\nSize of Char = %d",sizeof(char));
+    printf("\nSize of Float = %d",sizeof(float));
+    printf("\nSize of Double = %d",sizeof(double));
+
+    getch();
+    return;
+}
