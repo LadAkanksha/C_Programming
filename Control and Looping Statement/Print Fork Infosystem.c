4e359f75d854227468c1276faf989c3a20769459
LadAkanksha <akankshalad2003@gmail.com>
2024-08-17 11:34:14 +0530

    Looping Statement

---
+++ Control and Looping Statement/Print Fork Infosystem.c
@@ -0,0 +1,18 @@
+///Write Program to print “FORK” on first line & “INFOSYSTEMS” on second line.
+///Print this 5 times (use only one while loop).
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 1;
+    while(i <= 5)
+    {
+        printf("Fork\nInfosystems\n\n");
+        i++;
+    }
+    getch();
+    return 0;
+}
+
