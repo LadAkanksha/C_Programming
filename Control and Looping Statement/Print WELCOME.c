0de199841851c132dd203a87bb2d10e49b1ae6f6
LadAkanksha <akankshalad2003@gmail.com>
2024-08-17 11:23:57 +0530

    Looping Statement

---
+++ Control and Looping Statement/Print WELCOME.c
@@ -0,0 +1,14 @@
+///Write Program to print “WELCOME” message 10 times
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+    for( i = 1; i<= 10 ; i++)
+    {
+        printf("\nWelcome");
+    }
+    getch();
+    return 0;
+}
