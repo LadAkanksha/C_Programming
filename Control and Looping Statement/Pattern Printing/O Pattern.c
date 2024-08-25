d2131b6d22deef7aaa64c832203608bf6e6260d3
LadAkanksha <akankshalad2003@gmail.com>
2024-08-25 23:00:57 +0530

    Pattern Printing

---
+++ Control and Looping Statement/Pattern Printing/O Pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0 , j =  0 , Size = 0;
+
+    printf("\nEnter Size for Pattern : ");
+    scanf("%d" , &Size);
+    printf("\n");
+    for(i = 1 ; i<= Size ; ++i)
+    {        printf("\n");
+
+        for(j = 1 ; j<= Size ; ++j)
+        {
+            if(( j== 1 && j != i && i+j < Size+1) || (i == 1 && i != j && i+j < Size+1) || (j == Size && i+j > Size+1 && i != j) || ( i == Size && i+j > Size+1 && i != j ))
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
