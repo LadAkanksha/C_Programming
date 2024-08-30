7da13c3e8693448d9251e9b485a31f843905a32b
LadAkanksha <akankshalad2003@gmail.com>
2024-08-30 22:01:51 +0530

    Precedence and Associativity

---
+++ Basic/Operator Precedence/12 Increment Decrement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = -1;
+    +i;
+
+    printf("%d   %d  ",i ,+i);      //+ is unary plus and will not have any effect
+    getch();
+    return 0;
+}
+
+
