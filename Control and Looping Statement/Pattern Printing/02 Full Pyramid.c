29b45ba22092b4c2002f064cbeab7e4c88300973
LadAkanksha <akankshalad2003@gmail.com>
2024-09-03 21:44:45 +0530

    Pattern Printing

---
+++ Control and Looping Statement/Pattern Printing/02 Full Pyramid.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Size = 0, i = 0 , j = 0 , k = 0;
+
+    printf("\nEnter Size for Pattern : ");
+    scanf("%d",&Size);
+
+    for(i = 1 ; i <= Size ; i++)
+    {
+        for(j = 1 ; j <= 2*(Size - i)+1 ; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1 ; k <= (2*i)-1 ; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
