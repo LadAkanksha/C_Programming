e8d16ffc703a66664fc9332b3fd1a2406557838a
LadAkanksha <akankshalad2003@gmail.com>
2024-09-22 16:07:55 +0530

    Control Statement

---
+++ Control and Looping Statement/03 if statement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 10 , j = 20;
+
+    if( i = j)
+        printf("\ni and j are equal.");
+    else
+        printf("\ni and j are not equal");
+
+    getch();
+    return 0;
+}
