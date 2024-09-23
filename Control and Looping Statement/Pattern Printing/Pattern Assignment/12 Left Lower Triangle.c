9882224f0fd302d1d31b6935788bdf197050713d
LadAkanksha <akankshalad2003@gmail.com>
2024-09-23 19:52:37 +0530

    Print Pattern

---
+++ Control and Looping Statement/Pattern Printing/Pattern Assignment/12 Left Lower Triangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0 , j = 0 , Size = 0;
+
+    printf("\nEnter Size for Pattern : ");
+    scanf("%d",&Size);
+    printf("\n");
+    for( i = 1 ; i <= Size ; ++i )
+    {
+        for( j = 1 ; j <= Size ; ++j )
+        {
+            if( i == j || i >= j )
+            {
+                printf(" 7 ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
