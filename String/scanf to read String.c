8890cd54f01c0be249ac299713b1fba01aaa4e68
LadAkanksha <akankshalad2003@gmail.com>
2024-09-26 22:14:32 +0530

    String Program

---
+++ String/scanf to read String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char name[20];
+
+    printf("\nEnter Your Name : ");
+    scanf("%s",name);
+
+    printf("\nYour Name Is %s.",name);
+
+    getch();
+    return 0;
+}
