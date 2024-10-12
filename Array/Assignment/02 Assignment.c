def47f009c656dc52f2fbe15804cd1543ee46a99
LadAkanksha <akankshalad2003@gmail.com>
2024-10-12 22:12:32 +0530

    Array

---
+++ Array/Assignment/02 Assignment.c
@@ -0,0 +1,17 @@
+///Create array with initialization & display element.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[] = {45,21,76,39,90};
+
+    printf("\nArray element [0] = %d",Arr[0]);
+    printf("\nArray element [1] = %d",Arr[1]);
+    printf("\nArray element [2] = %d",Arr[2]);
+    printf("\nArray element [3] = %d",Arr[3]);
+    printf("\nArray element [4] = %d",Arr[4]);
+    getch();
+    return 0;
+}
