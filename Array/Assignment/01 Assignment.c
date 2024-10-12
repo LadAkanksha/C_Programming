7c1616c8ba66770c0be799583679cc687a11b641
LadAkanksha <akankshalad2003@gmail.com>
2024-10-12 22:12:09 +0530

    Array

---
+++ Array/Assignment/01 Assignment.c
@@ -0,0 +1,17 @@
+///Create array without initialization & display element.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[5];
+
+    printf("\nArray element [0] = %d",Arr[0]);
+    printf("\nArray element [1] = %d",Arr[1]);
+    printf("\nArray element [2] = %d",Arr[2]);
+    printf("\nArray element [3] = %d",Arr[3]);
+    printf("\nArray element [4] = %d",Arr[4]);
+    getch();
+    return 0;
+}
