15c067623cd2ccacf2320425562991e3b8194327
LadAkanksha <akankshalad2003@gmail.com>
2024-11-05 22:02:19 +0530

    Bitwise Operators

---
+++ Bitwise/Bitwise NOT.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 10 , d = 0;
+    d = ~i;
+    printf("\nValue of i = %d",i);
+    printf("\nValue in d = %d",d);
+
+    getch();
+    return 0;
+}
