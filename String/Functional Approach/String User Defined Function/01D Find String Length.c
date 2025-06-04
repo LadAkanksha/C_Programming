3a33b8d051aa43abe30c9718d395823a3a7ba126
LadAkanksha <akankshalad2003@gmail.com>
2025-06-04 23:15:03 +0530

    User Defined Function

---
+++ String/Functional Approach/String User Defined Function/01D Find String Length.c
@@ -0,0 +1,17 @@
+/// Calculate The Length of String
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[40] = "";
+    int length = 0;
+
+    length = printf("Akanksha Chandrakant Lad");
+
+    printf("\n\nLength of String is : %d" , length);
+
+    getch();
+    return 0;
+}
